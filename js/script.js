09b4cb5c6702ea5e9a2673d5595bc1c3e8e9535a
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-10 18:37:13 +0000

    Added Button event

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 // Recupero gli elementi in pagina
 
 const grid = document.getElementById('grid');
+const button = document.getElementById('button')
 
 // Funzioni
 
-const createCell = () => {
+// Funzione creazione cella
+
+const createCell = (content) => {
     const cell = document.createElement('div');
     cell.className = 'cell';
+    cell.innerText = content;
     return cell;
 }
 
@@ -18,11 +22,33 @@ const cols = 10;
 const totalCells = rows * cols;
 
 
+// Svolgimento delle funzioni dentro il click del bottone
+
+button.addEventListener('click', function () {
+
+    // Rimuovo il figlio di grid così che il loop riparta da capo
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    // Creo un ciclo for perchè mi servirà la i
+
+    for (let i = 1; i <= totalCells; i++) {
 
 
-for (let i = 1; i < totalCells; i++) {
+        // creo la cella e gli assegno il numero
+        const cell = createCell(i);
 
-    const cell = createCell();
+        // Aggiungo le funzioni al click
 
-    grid.appendChild(cell);
-}
+        cell.addEventListener('click', () => {
+            cell.classList.toggle('clicked');
+            // Quando clicco su una cella metto il numero in console
+            console.log('Cella cliccata numero:', i)
+        })
+
+        // Appendo la classe in pagina
+
+        grid.appendChild(cell);
+    }
+})
